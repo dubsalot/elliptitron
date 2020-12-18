99e12b9ef7b0bdab917b59ed872f98f490471e35
wes <wes.goodwin@gmail.com>
2020-12-18 01:23:14 -0600

    stuff

--- ui/gauge.js
+++ ui/gauge.js
@@ -1,4 +1,35 @@
+var net = require('net');
+var jQuery = require('jquery');
 
-function stuff(params) {
-    
-}
+jQuery(function () {
+    let degrees = 0;
+    let needle = $(".needle").first();
+
+    $('#btn').on("click", function () {
+        var client = net.connect({ port: 8484 }, function () {
+            console.log('Connection established!');
+        });
+
+        client.on('data', function (data) {
+            document.write(data.toString());
+            client.end();
+        });
+
+        client.on('end', function () {
+            console.log('Disconnected :(');
+        });
+        needle.css("transform", "rotate(" + degrees.toString() + "deg)");
+        console.log("hello");
+    });
+
+    // setInterval(() => {
+    //     if (degrees >= 179)
+    //     {
+    //         degrees = 0;
+    //     }
+    //     else { 
+    //         degrees += 5;
+    //     }
+    //     needle.css("transform", "rotate(" + degrees.toString() + "deg)");            
+    // }, 500);
+});
