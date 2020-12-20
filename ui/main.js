e3a08710ff2d9f4eb0117f89c61cb236095cf3f6
wes <wes.goodwin@gmail.com>
2020-12-19 22:18:53 -0600

    who knows

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ function createWindow () {
   })
 
   //win.loadFile('gauge/gauge.html')
-  win.loadFile('proto/index.html')
+  win.loadFile('proto/proto.html')
 }
 
 app.whenReady().then(createWindow)
